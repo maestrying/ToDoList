7e852dc336b5a19565c7ef12993dd59b155d0ba5
maestrying <mdwork96@gmail.com>
2024-04-19 11:30:32 +1000

    Created base project

---
+++ ToDoList/ToDoList/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  ToDoList
+//
+//  Created by Дывак Максим on 19.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+

0bc14eecf7e0c116c1d20fcc4adf6c0682b7ea3e
maestrying <mdwork96@gmail.com>
2024-04-19 12:07:29 +1000

    Added the functionality of the main screen

--- ToDoList/ToDoList/MainViewController.swift
+++ ToDoList/ToDoList/MainViewController.swift
@@ -7,14 +7,75 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+// MARK: - Protocold
+protocol AddTaskViewControllerDelegate: AnyObject {
+    func createTask(text: String)
+}
 
+
+// MARK: - Data
+struct DataToDo {
+    var title: String
+}
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - Data initial
+    var dataArr: [DataToDo] = []
+    
+    
+    // MARK: - Properties
+    var tableView: UITableView!
+    let identifier = "MyTable"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemMint
+        setupLayout()
+    }
+    
+    
+    // MARK: - methods
+    private func createTable() {
+        let tableView = UITableView(frame: view.bounds)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        view.backgroundColor = .white
+        
+        title = "ToDo-Lite"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTask))
+    
+        createTable()
+    }
+    
+    
+    // MARK: - Table methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = dataArr[indexPath.row].title
+        return cell
+    }
+    
+    @objc func addTask() {
+        
     }
 
 
 }
+
+
+
+
 
